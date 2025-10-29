98c386f29e4cb4ce75c00bebc65dd57219b575a0
Headliner770 <paul248022@gmail.com>
2025-10-29 10:21:35 +0300

    sum-arrays.js  solution

--- 8kyu/sum-arrays.js
+++ 8kyu/sum-arrays.js
@@ -1,5 +1,6 @@
 // ❓ Description:
 // Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+// Напишите функцию, которая принимает массив чисел и возвращает их сумму. Числа могут быть отрицательными или нецелыми. Если массив не содержит чисел, следует вернуть 0.
 
 // Examples
 // Input: [1, 5.2, 4, 0, -1]
@@ -20,3 +21,8 @@
 // Advanced users may find this extremely easy and can easily write this in one line.
 
 // ✔️ SOLUTION:
+const numbers = [1, 5.2, 4, 0, -1];
+function sum(numbers) {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
+console.log(sum(numbers));
